da0df48f2c68bffb2bd36b003ec3afce82d5632e
AryaveerSR <me.aryaveer@gmail.com>
2026-02-26 15:45:53 +0530

    add telemetry

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,16 @@
-use axum::Router;
+use axum::{Router, extract::Request};
 use axum_macros::FromRef;
 use sqlx::PgPool;
+use tower::Layer;
+use tower_http::{
+    normalize_path::{NormalizePath, NormalizePathLayer},
+    trace::TraceLayer,
+};
+use tracing::Level;
+use uuid::Uuid;
 
 mod config;
 mod routes;
-
 pub use config::CONFIG;
 
 #[derive(Clone, FromRef)]
@@ -12,10 +18,21 @@ pub struct AppState {
     pool: sqlx::PgPool,
 }
 
-pub fn app(pool: PgPool) -> Router {
+pub fn app(pool: PgPool) -> NormalizePath<Router> {
     let state = AppState { pool };
 
-    Router::new()
+    let svc = Router::new()
         .nest("/api", routes::routes())
         .with_state(state)
+        .layer(TraceLayer::new_for_http().make_span_with(|req: &Request| {
+            tracing::span!(
+                Level::DEBUG,
+                "request",
+                method = %req.method(),
+                uri = %req.uri(),
+                req_id = %Uuid::new_v4().to_string().split('-').next_back().unwrap(),
+            )
+        }));
+
+    NormalizePathLayer::trim_trailing_slash().layer(svc)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
+use axum::{ServiceExt, extract::Request};
 use forum::CONFIG;
 use sqlx::PgPool;
 use tokio::net::TcpListener;
+use tracing::info;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
+    tracing_subscriber::fmt::init();
+
     let pool = PgPool::connect(&CONFIG.database.conn_string())
         .await
         .expect("Failed to connect to database.");
@@ -11,5 +15,7 @@ async fn main() -> Result<(), std::io::Error> {
     let app = forum::app(pool);
     let listener = TcpListener::bind(format!("0.0.0.0:{}", CONFIG.port)).await?;
 
-    axum::serve(listener, app).await
+    info!("Starting server on port {}.", CONFIG.port);
+
+    axum::serve(listener, ServiceExt::<Request>::into_make_service(app)).await
 }
