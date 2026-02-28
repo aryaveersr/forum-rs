b9b46698594e8662e8a865246fec88db395318e1
AryaveerSR <me.aryaveer@gmail.com>
2026-02-28 14:07:14 +0530

    add foreign key constraint on sessions

--- migrations/20260227170911_create_sessions_table.sql
+++ migrations/20260227170911_create_sessions_table.sql
@@ -1,6 +1,6 @@
 -- Create sessions table
 CREATE TABLE sessions (
     id UUID PRIMARY KEY,
-    user_id UUID NOT NULL,
+    user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
     expires_at TIMESTAMPTZ NOT NULL
 );
